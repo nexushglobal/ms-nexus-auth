8bfa695b7db67bc0cd4f91e9fc4e31de32433209
JosmenDev <josmendev@gmail.com>
2025-07-07 09:48:55 -0500

    feat(filters): added validation filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CommonModule],
 })
 export class AppModule {}
